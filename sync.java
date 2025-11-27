72500eaa4de810b5e3496643628a1d20d881220f
Bogdan Pop-Cîmpean <8363210+bogdanpc@users.noreply.github.com>
2025-11-27 16:56:45 +0200

    Fix running jbang with the sync app command line parameters

--- sync.java
+++ sync.java
@@ -1,18 +1,41 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 25+
-//DEPS io.quarkus.platform:quarkus-bom:3.28.1@pom
+//DEPS io.quarkus.platform:quarkus-bom:3.30.1@pom
 //DEPS io.quarkus:quarkus-picocli
 //DEPS io.quarkus:quarkus-rest-client-jackson
 //DEPS io.quarkus:quarkus-arc
-//JAVAC_OPTIONS -parameters
 //JAVA_OPTIONS -Djava.util.logging.manager=org.jboss.logmanager.LogManager
 //JAVA_OPTIONS --add-opens=java.base/java.lang=ALL-UNNAMED
 //SOURCES src/main/java/**/*.java
 //FILES src/main/resources/application.properties
-//Q:CONFIG quarkus.banner.enabled=false
+
+
+import bogdanpc.linearsync.cli.boundary.LinearJiraSyncCommand;
+
+import picocli.CommandLine;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.picocli.runtime.annotations.TopCommand;
+import jakarta.inject.Inject;
 
-void main(String... args) {
-    Quarkus.run(args);
+@QuarkusMain
+public class sync implements QuarkusApplication {
+
+    @Inject
+    CommandLine.IFactory factory;
+
+    @Inject
+    @TopCommand
+    LinearJiraSyncCommand command;
+
+    @Override
+    public int run(String... args) throws Exception {
+        return new CommandLine(command, factory).execute(args);
+    }
+
+    public static void main(String... args) {
+        Quarkus.run(sync.class, args);
+    }
 }
