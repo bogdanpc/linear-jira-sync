c8c76894f6758819941b82e851c54b5b4ee2d1af
Bogdan Pop-Cîmpean <8363210+bogdanpc@users.noreply.github.com>
2026-03-03 22:53:50 +0200

    Bump Quarkus to 3.32.1

--- sync.java
+++ sync.java
@@ -1,14 +1,14 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 25+
-//DEPS io.quarkus.platform:quarkus-bom:3.30.1@pom
+//DEPS io.quarkus.platform:quarkus-bom:3.32.1@pom
 //DEPS io.quarkus:quarkus-picocli
 //DEPS io.quarkus:quarkus-rest-client-jackson
 //DEPS io.quarkus:quarkus-arc
+//JAVA_OPTIONS -XX:+UseCompactObjectHeaders
 //JAVA_OPTIONS -Djava.util.logging.manager=org.jboss.logmanager.LogManager
 //JAVA_OPTIONS --add-opens=java.base/java.lang=ALL-UNNAMED
 //SOURCES src/main/java/**/*.java
 //FILES src/main/resources/application.properties
-
 
 import bogdanpc.linearsync.cli.boundary.LinearJiraSyncCommand;
 
@@ -23,19 +23,19 @@ import jakarta.inject.Inject;
 @QuarkusMain
 public class sync implements QuarkusApplication {
 
-    @Inject
-    CommandLine.IFactory factory;
+  @Inject
+  CommandLine.IFactory factory;
 
-    @Inject
-    @TopCommand
-    LinearJiraSyncCommand command;
+  @Inject
+  @TopCommand
+  LinearJiraSyncCommand command;
 
-    @Override
-    public int run(String... args) throws Exception {
-        return new CommandLine(command, factory).execute(args);
-    }
+  @Override
+  public int run(String... args) throws Exception {
+    return new CommandLine(command, factory).execute(args);
+  }
 
-    public static void main(String... args) {
-        Quarkus.run(sync.class, args);
-    }
+  public static void main(String... args) {
+    Quarkus.run(sync.class, args);
+  }
 }
